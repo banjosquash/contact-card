4e30fa6243a3f024971b323f40ec7528cfd682c3
Brayden <banjosquash@gmail.com>
2022-11-12 10:31:38 -0500

    made changes to form/index.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,43 +1,41 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
+const path = require('path');
 
 module.exports = {
-    entry: "./src/js/index.js",
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    mode: 'development',
-    module: {
-        rules: [
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  ['@babel/preset-env', { targets: "defaults" }]
-                ]
-              }
-            }
-          }
-        ]
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Webpack Plugin',
+    }),
+
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-      plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            title: 'Webpack Plugin'
-        })
-      ]
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
