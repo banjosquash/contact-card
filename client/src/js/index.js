3e02c89bf2876f6fbc7d03c42b6c532ee25fa3cb
Brayden <banjosquash@gmail.com>
2022-11-11 20:51:33 -0500

    connected to indexDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,8 +10,10 @@ import "../css/main.css";
 
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initdb } from './database';
 
 window.addEventListener('load', function () {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
